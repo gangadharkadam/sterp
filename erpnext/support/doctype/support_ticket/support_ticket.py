0b9f78609f0c119e58eaf786949c276dffe965fd
gangadhar kadam <gangadhar.k@gmail.com>
2014-08-25 13:18:09 +0530

    for push upstream

--- erpnext/support/doctype/support_ticket/support_ticket.py
+++ erpnext/support/doctype/support_ticket/support_ticket.py
@@ -142,40 +142,7 @@ def assing_future(name, assign_in_future,raised_by,assign_to):
 			})
 			am.insert()
 
-
 def auto_close_tickets():
-<<<<<<< HEAD
 	frappe.db.sql("""update `tabSupport Ticket` set status = 'Closed'
 		where status = 'Replied'
 		and date_sub(curdate(),interval 15 Day) > modified""")
-
-
-@frappe.whitelist()
-def assing_future(name, assign_in_future,raised_by,assign_to):
-  	#frappe.errprint("in assign")
-  	from frappe.utils import get_url, cstr
-	if get_url()=='http://smarttailor':
-		check_entry = frappe.db.sql("""select assign_to from `tabAssing Master` where name = %s """, raised_by)
-		#frappe.errprint("in assign")
-		if check_entry :
-			#frappe.errprint("chk")
-			if assign_in_future=='No':
-				#frappe.errprint("no")
-				frappe.db.sql("""delete from `tabAssing Master` where name = %s """, raised_by)	
-			else :
-				#frappe.errprint("Yes")
-				frappe.db.sql("""update `tabAssing Master` set assign_to=%s where name = %s """,(assign_to,raised_by))
-		else :
-			#frappe.errprint("not chk")
-			if assign_in_future=='Yes':
-				#frappe.errprint("Yes")
-				am = frappe.new_doc("Assing Master")
-				am.update({
-				"name": raised_by,
-				"assign_to": assign_to,
-				"raised_by":raised_by
-				})
-				am.insert()
-=======
-	frappe.db.sql("""update `tabSupport Ticket` set status = 'Closed' where status = 'Replied' and date_sub(curdate(),interval 15 Day) > modified""")
->>>>>>> 739c565385f73c3d0b5a106177afa57f23b05eed
