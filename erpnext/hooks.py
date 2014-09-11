c0ff7c59e4ff8a75a8237aa186819a1f7a2e1eb2
gangadhar kadam <gangadhar.k@gmail.com>
2014-09-11 20:27:48 +0530

    for push upstream

--- erpnext/hooks.py
+++ erpnext/hooks.py
@@ -8,8 +8,8 @@ app_color = "#e74c3c"
 app_version = __version__
 
 error_report_email = "support@erpnext.com"
-
-app_include_js = ["assets/js/erpnext.min.js","assets/js/chart.js","assets/js/sitevisit.js"]
+#app_include_js = ["assets/js/erpnext.min.js","assets/js/sitevisit.js"]
+app_include_js = ["assets/js/erpnext.min.js","assets/js/chart.js"]
 app_include_css = "assets/css/erpnext.css"
 web_include_js = "assets/js/erpnext-web.min.js"
 
